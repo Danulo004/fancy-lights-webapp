af339addd02149b1efad33d236248bbdea00eb73
Danulo004 <danulo004@gmail.com>
2021-09-27 01:44:02 +0300

    Added styled component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import '../App.css';
+
+const Text = styled.p`
+  color: palevioletred;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <Text>
           Edit
           <code> src/App.tsx </code>
           and save to reload.
-        </p>
+        </Text>
         <a
           className="App-link"
           href="https://reactjs.org"
